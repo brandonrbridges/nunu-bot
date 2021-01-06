f449858e170c2590b95f7e4ce2b19a024e3d4c26
Brandon Bridges <brandon@visually.digital>
2021-01-06 00:12:08 +0000

    temporary guildmemberadd and guildmemberremove listeners

---
+++ listeners/guildMemberAdd.js
@@ -0,0 +1,18 @@
+const { Listener } = require('discord-akairo')
+
+const { getAvatarUrl } = require('../functions/helpers')
+
+const { MessageEmbed } = require('discord.js')
+
+module.exports = class GuildMemberAddListener extends Listener {
+    constructor() {
+        super('guildMemberAdd', {
+            emitter: 'client',
+            event: 'guildMemberAdd'
+        })
+    }
+
+    exec(member) {
+        console.log(member)
+    }
+}
---
+++ listeners/guildMemberRemove.js
@@ -0,0 +1,18 @@
+const { Listener } = require('discord-akairo')
+
+const { getAvatarUrl } = require('../functions/helpers')
+
+const { MessageEmbed } = require('discord.js')
+
+module.exports = class GuildMemberRemoveListener extends Listener {
+    constructor() {
+        super('guildMemberRemove', {
+            emitter: 'client',
+            event: 'guildMemberRemove'
+        })
+    }
+
+    exec(member) {
+        console.log(member)
+    }
+}
