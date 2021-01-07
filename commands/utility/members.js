84554485539c21c24ae87fdd21787e631c29804e
Brandon Bridges <brandon@visually.digital>
2021-01-07 20:28:20 +0000

    create members command

--- commands/utility/members.js
+++ commands/utility/members.js
@@ -1,5 +1,9 @@
 const { Command } = require('discord-akairo')
 
+const { getAvatarUrl } = require('../../functions/helpers')
+
+const { MessageEmbed } = require('discord.js')
+
 module.exports = class MembersCommand extends Command {
     constructor() {
         super('members', {
@@ -8,6 +12,17 @@ module.exports = class MembersCommand extends Command {
     }
 
     exec(message) {
-        
+        // Create embed
+        const embed = new MessageEmbed({
+            color: '#ffa801',
+            description: `${message.guild.name} has ${message.guild.memberCount} members!`,
+            footer: {
+                iconURL: getAvatarUrl(client.user),
+                text: client.user.username
+            }
+        }).setTimestamp()
+
+        // Send embed
+        return message.channel.send(embed)
     }
 }
