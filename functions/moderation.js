3bbec25aa3fe07540f4704a3d061e1e345ce4e96
Brandon Bridges <brandon@visually.digital>
2021-01-07 20:20:40 +0000

    added base moderation function

--- functions/moderation.js
+++ functions/moderation.js
@@ -1,4 +1,18 @@
 /**
+ * Ban Member
+ * 
+ * @description Bans a member from the guild with a reason
+ * 
+ * @argument member @type GuildMember
+ * @argument reason @type String
+ * 
+ * @version 1.0.0
+ */
+const banMember = (member, reason) => {
+    
+}
+
+/**
  * Kick Member
  * 
  * @description Kicks a member from the guild with a reason
@@ -9,6 +23,34 @@
  * @version 1.0.0
  */
 const kickMember = (member, reason) => {
+    
+}
+
+/**
+ * Softban Member
+ * 
+ * @description Softbans a member from the guild with a reason
+ * 
+ * @argument member @type GuildMember
+ * @argument reason @type String
+ * 
+ * @version 1.0.0
+ */
+const softbanMember = (member, reason) => {
+    
+}
+
+/**
+ * Warn Member
+ * 
+ * @description Warns a member from the guild with a reason
+ * 
+ * @argument member @type GuildMember
+ * @argument reason @type String
+ * 
+ * @version 1.0.0
+ */
+const warnMember = (member, reason) => {
     
 }
 
