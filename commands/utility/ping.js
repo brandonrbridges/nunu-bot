ae364478eb698e7fc4a6ef6b7b0c0e1d48db0f20
Brandon Bridges <brandon@visually.digital>
2021-01-07 23:58:19 +0000

    update ping

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -1,5 +1,9 @@
 const { Command } = require('discord-akairo')
 
+const { getAvatarUrl } = require('../../functions/helpers')
+
+const { MessageEmbed } = require('discord.js')
+
 module.exports = class PingCommand extends Command {
     constructor() {
         super('ping', {
@@ -9,6 +13,16 @@ module.exports = class PingCommand extends Command {
     }
 
     exec(message) {
-        return message.channel.send('pong')
+        // Create embed
+        const embed = new MessageEmbed({
+            description: 'Pong!',
+            footer: {
+                iconURL: getAvatarUrl(client.user),
+                text: client.user.username
+            }
+        }).setTimestamp()
+
+        // Send embed
+        return message.channel.send(embed)
     }
 }
