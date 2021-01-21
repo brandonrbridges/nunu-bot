61a8b427499a399f22fe3d8c6d022dbc5e0e1252
Brandon Bridges <brandon@visually.digital>
2021-01-21 21:22:06 +0000

    ban command adjustments

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -1,11 +1,10 @@
 // Akairo
 const { Command } = require('discord-akairo')
 
+// Mongoose
+const User = require('../../database/schema/user') 
+
 // Functions
-const {
-
-} = require('../../functions/moderation')
-
 const {
     checkPermissions,
     embedConsoleError,
@@ -25,21 +24,40 @@ module.exports = class BanCommand extends Command {
                 {
                     id: 'reason',
                     type: 'string'
+                },
+                {
+                    id: 'force',
+                    match: 'flag',
+                    flag: '--force'
                 }
             ]
         })
     }
 
-    async exec(message, { member, reason }) {
+    async exec(message, { member, reason, force }) {
         const permission = await checkPermissions(message, 'BAN_MEMBERS')
+
+        const user = await User.findOne({ discordId: member.id })
 
         try {
             if(permission) {
                 if(member.bannable) {
-                    member.ban({ reason })
+                    if(force) {
+                        member.ban({ reason })
 
-                    const embed = embedSuccess(`👋 ${member} has been banned from ${message.guild.name} by ${message.author}!`)
-                    return message.channel.send(embed)
+                        const embed = embedSuccess(`👋 ${member} was force banned from ${message.guild.name} by ${message.author}!`)
+                        return message.channel.send(embed)
+                    }
+
+                    if(user.infractions.length >= 3) {
+                        member.ban({ reason })
+
+                        const embed = embedSuccess(`👋 ${member} has been banned from ${message.guild.name} by ${message.author}!`)
+                        return message.channel.send(embed)
+                    } else {
+                        const embed = embedError(`${message.author}, I cannot ban ${member} until they have had at least 3 warnings!`)
+                    return message.channel.send(embed)    
+                    }
                 } else {
                     const embed = embedError(`${message.author}, you cannot ban ${member}!`)
                     return message.channel.send(embed)
