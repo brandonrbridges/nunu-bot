da90e0c8f4f27cfa829db866a705c37acd0943bf
HammerToTheKeyboard <43912810+HammerToTheKeyboard@users.noreply.github.com>
2021-01-24 04:03:07 +0100

    removed xp limiter

--- index.js
+++ index.js
@@ -1,121 +1,108 @@
-require('dotenv').config()
+const User = require('../database/schema/user')
 
-const { AkairoClient, CommandHandler, ListenerHandler } = require('discord-akairo')
+const { embedSuccess } = require('./helpers')
 
-const mongoose = require('mongoose')
+const { levels } = require('../database/levels')
 
-const cron = require('node-cron')
+/**
+ * Adds experience
+ * 
+ * @description Adds user experience
+ * 
+ * @version 1.1.0
+ */
+const addExperience = async (discordId,content) => {
+    try {
+        const user = await User.findOne({ discordId })
 
-const { resetDailies } = require('./functions/currency')
-const { resetExperienceGain } = require('./functions/levelling')
+        if(user && user.canEarnExperience) {
+            const cappedXP = Math.floor(50 - 10** (-(content.length-860)/507.5)) 
 
-const CustomGame = require('./database/schema/customgame')
-
-prefix = '!'
-
-class Client extends AkairoClient {
-    constructor() {
-        super({
-            // Akairo Settings
-            ownerID: ['471841188341743616'],
-        }, {
-            // Discord.js Settings
-            disableMentions: 'everyone'
-        })
-
-        /**
-         * Command Handlers
-         */
-
-        this.commands = new CommandHandler(this, {
-            directory: './commands/',
-            prefix: prefix
-        }).loadAll()
-
-        /**
-         * Listener Handler
-         */
-        this.listeners = new ListenerHandler(this, {
-            directory: './listeners/'
-        }).loadAll()
-    }   
+            await User.findOneAndUpdate({ discordId }, { $inc: { experience: cappedXp } }, { new: true })
+            
+        } else { 
+            return
+        }
+    } catch(error) {
+        return console.error(error) 
+    }
 }
 
-client = new Client()
-
 /**
- * DATABASE CONNECTION
+ * Check XP
+ * 
+ * @description Checks user experience count 
  * 
  * @version 1.0.0
  */
-mongoose.connect(process.env.DATABASE, { useFindAndModify: false, useNewUrlParser: true, useUnifiedTopology: true })
-.then(() => {
-    console.log('== DATABASE CONNECTED ==')
-})
-.catch(error => {
-    console.error('== ! UNABLE TO CONNECT TO DATABASE ! ==')
-    console.error(error)
-})
+const checkXp = async (discordId, guild, channel) => {
+    try {
+        const user = await User.findOne({ discordId })
+
+        if(user) {
+            if(user.experience >= levels[user.level + 1].xp) {
+                levelUp(discordId, guild, channel)
+            } else {
+                return
+            }
+        } else {
+            return
+        }
+    } catch(error) {
+        return console.error
+    }
+}
 
 /**
- * Cron Job 
+ * Level up
  * 
- * @description Runs at midnight to see if the user has used the daily command
- */
-cron.schedule('0 0 * * *', () => {
-    // Reset user daily usage
-    resetDailies()
-})
-
-/**
- * Cron Job
- * 
- * @description Runs every minute to check if the user has sent a message and gained XP
- */
-cron.schedule('* * * * *', () => {
-    // Resets the ability to gain XP to true
-    resetExperienceGain()
-})
-
-/**
- * Cron Job
- * 
- * @description Runs at 7pm everyday and creates a server wide custom game
- */
-cron.schedule('55 18 * * *', () => {
-
-    const guild = client.guilds.cache.get('788731111845003294')
-    const channel = guild.channels.cache.find(channel => channel.name == 'general')
-    const role = guild.roles.cache.find(role => role.name == 'Custom Games')
-
-    const embed = embedStandard(` 🕹️ Players be ready! Our Custom Games are starting in 5 minutes!`)
-
-    channel.send(role)
-    return channel.send(embed)
-})
-
-cron.schedule('00 19 * * *', () => {
-
-    const guild = client.guilds.cache.get('788731111845003294')
-    const channel = guild.channels.cache.find(channel => channel.name == 'general')
-    const role = guild.roles.cache.find(role => role.name == 'Custom Games')
-
-    const embed = embedStandard(`🕹️ Let the Custom Games begin! Let's go!`)
-
-    channel.send(role)
-    return channel.send(embed)
-})
-
-/**
- * Discord Bot Login
+ * @description Increases user level
  * 
  * @version 1.0.0
  */
-client.login(process.env.TOKEN)
-.then(() => {
-    console.log(`== ${client.user.username.toUpperCase()} IS SIGNED IN ==`)
-})
-.catch(error => {
-    console.log('== ! UNABLE TO CONNECT TO DISCORD ! ==')
-    console.error(error)
-})
+const levelUp = async (discordId, guild, channel) => {
+    try {
+        const user = await User.findOne({ discordId })
+        const updatedUser = await User.findOneAndUpdate({ discordId }, { level: user.level + 1, experience: 0 }, { new: true })
+
+        const member = guild.members.cache.get(discordId)
+
+        if(levels[updatedUser.level].hasRole) {
+            const role = guild.roles.cache.find(role => role.name === `Level ${updatedUser.level}`)
+            member.roles.add(role)
+        }
+
+        const embed = embedSuccess(`🎉 ${member} has levelled up to Level ${updatedUser.level}`)
+        return channel.send(embed)
+    } catch(error) {
+        return console.error
+    }
+}
+
+/**
+ * Reset Experience
+ * 
+ * @description Resets everyone's experience so that they can earn again
+ * 
+ * @version 1.0.0
+ */
+const resetExperienceGain = async () => {
+    try {
+        const users = await User.find({ canEarnExperience: false })
+
+        users.forEach(async user => {
+            await User.findOneAndUpdate({ discordId: user.discordId }, { canEarnExperience: true }, { new: true })
+        })
+    } catch(error) {
+        return console.error(error)
+    }
+}
+
+/**
+ * Exports
+ */
+module.exports = {
+    addExperience,
+    checkXp,
+    resetExperienceGain
+}
